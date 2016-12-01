828bba5195600fc8edf62423f36ffe72830538e8
jasonsmeaton <jasonsmeaton@gmail.com>
2016-12-01 17:18:51 +0000

    package.json: version number and url

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import {Route} from "react-router";
-import Home from "./components/home";
+import { Route, IndexRoute } from "react-router";
+import { Home } from "./components/home";
+import { ElectrodeTestComponent } from "electrode-test-component-54321";
 
 export const routes = (
-  <Route path="/" component={Home}/>
+  <Route path="/" component={Home}>
+    <IndexRoute component={ElectrodeTestComponent}/>
+    <Route path="/invite" component={ElectrodeTestComponent}/>
+  </Route>
 );
